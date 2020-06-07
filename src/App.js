26c95eb3dc04efaecf43087fbc4c890fd7da3f69
eatnows <gagyasindev@gmail.com>
2020-06-07 21:14:32 +0900

    dev 브랜치 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Signup from "./components/signup/signup";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
 }
 
 export default App;
