0fa9e210a7e815db3ccb766b0ddd3897d8462629
eatnows <gagyasindev@naver.com>
2020-06-11 15:41:12 +0900

    카카오 지도 라이브러리 추가 ref #12

---
+++ src/pages/ReviewAdd.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WriteFormMap from "../components/map/WriteFormMap";
+
+const ReviewAdd = () => {
+  return (
+    <div>
+      <WriteFormMap />;
+    </div>
+  );
+};
+
+export default ReviewAdd;
--- src/shared/App.js
+++ src/shared/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Home } from "pages";
 import Menu from "components/Menu";
+import { ReviewAdd } from "../pages";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <Menu />
         <Route exact path="/" component={Home} />
+        <Route exact path="/reviewadd" component={ReviewAdd} />
         <Switch></Switch>
       </div>
     );
