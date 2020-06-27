b713f25f67827dc9063744e3dc542bb86dec45b7
eatnows <gagyasindev@gmail.com>
2020-06-27 16:57:14 +0900

    지도 버그 수정 ref #30
    로고 클릭으로 메인지도로 페이지 이동시 지도위에 마커가 안보이는 버그 수정

--- src/components/map/MainMap.js
+++ src/components/map/MainMap.js
@@ -9,7 +9,6 @@ const MainMap = (props) => {
   const [latitude, setLatitude] = useState(37.49879629938368);
   const [longitude, setLongitude] = useState(127.03168720284643);
   const [map, setMap] = useState("");
-  const [overlay, setOverlay] = useState("");
   const [userId, setUserId] = useState(sessionStorage.getItem("userId"));
   // 나의 맛집 리스트들 필요한 변수
   const [myLatitude, setMyLatitude] = useState([]);
@@ -25,43 +24,8 @@ const MainMap = (props) => {
   const [myUserId, setMyUserId] = useState([]);
   const [myCategory, setMyCategory] = useState([]);
   const [myBlackList, setMyBlackList] = useState([]);
-  const useTest = useRef();
-  const [isOpen, setIsOpen] = useState(true);
-  const [markers, setMarkers] = useState([]);
-  // 내 친구들의 맛집 리스트들 필요한 변수
-  const [friendLatitude, setFriendLatitude] = useState([]);
-  const [friendLongitude, setFriendLongitude] = useState([]);
-  const [friendPlaceName, setFriendPlaceName] = useState([]);
-  const [friendPlaceAddress, setFriendPlaceAddress] = useState([]);
-  const [friendSubject, setFriendSubject] = useState([]);
-  const [friendContent, setFriendContent] = useState([]);
-  const [friendRating, setFriendRating] = useState([]);
-  const [friendFrontImg, setFriendFrontImg] = useState([]);
-  const [friendUPId, setFriendUPId] = useState([]);
-  const [friendUserId, setFriendUserId] = useState([]);
-  const [friendPinUrl, setFriendPinUrl] = useState([]);
-  const [friendMarkers, setFriendMarkers] = useState([]);
-  // 마커에서 맛집 정보 출력할때 필요한 변수들
-  const [subject, setSubject] = useState("");
-  const [content, setContent] = useState("");
-  const [rating, setRating] = useState("");
-  const [postCount, setPostCount] = useState("");
-  const [placeName, setPlaceName] = useState("");
-  // 리뷰글로 넘어갈때 필요한 변수
-  const [UPId, setUPid] = useState("");
 
-  const [category, setCategory] = useState(["한식", "양식", "중식", "일식"]);
-  //const [closeBtn, setCloseBtn] = useState();
-  //const [myFilterRender, setMyFilterRender] = useState(true);
-  useEffect(() => {
-    console.log(props.blacklistFilter);
-    //setMyFilterRender(props.MyFilter);
-  }, [
-    props.MyFilter,
-    props.FriendFilter,
-    props.categoryFilter,
-    props.blacklistFilter,
-  ]);
+  const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
     const url = `/place/allplace?user_id=${sessionStorage.getItem("userId")}`;
@@ -95,30 +59,6 @@ const MainMap = (props) => {
       .catch((error) => {
         console.log(error);
       });
-
-    /*
-     * 친구들의 맛집 정보를 받는 RESTful API
-     */
-
-    // Axios.get(friendUrl)
-    //   .then((res) => {
-    //     for (let i = 0; i < res.data.length; i++) {
-    //       friendLatitude.push(res.data[i].latitude_x);
-    //       friendLongitude.push(res.data[i].longitude_y);
-    //       friendPlaceName.push(res.data[i].name);
-    //       friendPlaceAddress.push(res.data[i].address);
-    //       friendSubject.push(res.data[i].subject);
-    //       friendContent.push(res.data[i].content);
-    //       friendRating.push(res.data[i].rating);
-    //       friendFrontImg.push(res.data[i].front_image);
-    //       friendUPId.push(res.data[i].user_place_id);
-    //       friendUserId.push(res.data[i].user_id);
-    //       friendPinUrl.push(res.data[i].pin_url);
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
   }, []);
   useEffect(() => {
     const script = document.createElement("script");
@@ -136,9 +76,153 @@ const MainMap = (props) => {
         };
         // 지도를 생성합니다
         createmap = new kakao.maps.Map(mapContainer, mapOption);
+
+        setTimeout(() => {
+          markerLoad();
+        }, 50);
       });
     };
   }, []);
+
+  const markerLoad = () => {
+    /*
+     * 기존의 맛집 마커를 삭제
+     */
+    for (let i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    setMap(createmap);
+
+    // 이미지 태그 제거
+    let imgTag = /<IMG(.*?)>/gi;
+
+    let marker = [];
+    let content = [];
+    let pageBtn = [];
+    let closeTag = [];
+    /*
+     * 지도의 나의 맛집 출력
+     */
+    for (let i = 0; i < myPlaceName.length; i++) {
+      // 나의 맛집 표시핀
+      let imageSrc = myPinUrl[i], // 마커이미지의 주소입니다
+        // 마커이미지의 크기입니다
+        imageSize = new kakao.maps.Size(64, 69),
+        // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+        imageOption = { offset: new kakao.maps.Point(27, 69) };
+      if (blacklistFilter(myUserId[i], myBlackList[i])) {
+        imageSrc =
+          "https://mechelinbucket.s3.ap-northeast-2.amazonaws.com/images/profle/blacklist_marker.png";
+      }
+
+      // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+      let markerImage = new kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize,
+          imageOption
+        ),
+        markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
+
+      marker[i] = new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(myLatitude[i], myLongitude[i]),
+        image: markerImage,
+      });
+
+      /*
+       * 오버레이에 들어갈 내용 만들기
+       */
+      // 이미지와 내용이 들어가는 오버레이
+      pageBtn[i] = document.createElement("div");
+      pageBtn[i].setAttribute("className", "page");
+      // 닫기버튼 오버레이
+      closeTag[i] = document.createElement("div");
+      closeTag[i].insertAdjacentHTML(
+        "afterbegin",
+        '<div class="wrapTitle"><div class="infoTitle"><div class="title2">' +
+          myPlaceName[i] +
+          '<div class="close" title="닫기"></div></div></div></div>'
+      );
+
+      document.body.appendChild(pageBtn[i]);
+
+      pageBtn[i].setAttribute("style", "width: 10px; height: 10px;");
+      content[i] =
+        '<div class="wrap">' +
+        '    <div class="info">' +
+        '        <div class="title">' +
+        "        </div>" +
+        '        <div class="body">' +
+        '            <div class="img">' +
+        '                <img src="' +
+        myFrontImg[i] +
+        '" width="73px" height="70px">' +
+        "           </div>" +
+        '            <div class="desc">' +
+        '                <div class="ellipsis">' +
+        mySubject[i] +
+        "</span>" +
+        "</div>" +
+        '                <div class="jibun ellipsis">' +
+        myContent[i].replace(imgTag, "").substring(0, 15) +
+        "</div>" +
+        '                <span title="리뷰글로 이동" class="moreBtn">...더보기</span>' +
+        "<span style='float: right; margin-bottom: 20px;'>평점: " +
+        myRating[i] +
+        "점&nbsp;&nbsp;" +
+        "            </div>" +
+        "        </div>" +
+        "    </div>" +
+        "</div>";
+
+      // 마커 위에 커스텀오버레이를 표시합니다
+      // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+
+      let overlay = [];
+      let overlay2 = [];
+
+      pageBtn[i].insertAdjacentHTML("afterbegin", content[i]);
+      overlay[i] = new kakao.maps.CustomOverlay({
+        content: pageBtn[i],
+        map: null,
+        position: marker[i].getPosition(),
+      });
+      overlay2[i] = new kakao.maps.CustomOverlay({
+        content: closeTag[i],
+        map: null,
+        position: marker[i].getPosition(),
+        xAnchor: 0.3,
+        yAnchor: 0.91,
+      });
+
+      /*
+       * 오버레이를 클릭했을때 리뷰 페이지로 이동
+       */
+      pageBtn[i].onclick = () => {
+        //overlay[i].setMap(null);
+        props.history.push(`/mechelin/review/${myUPId[i]}`);
+      };
+
+      /*
+       * 오버레이2를 클릭했을때 오버레이 삭제
+       */
+      closeTag[i].onclick = () => {
+        overlay[i].setMap(null);
+        overlay2[i].setMap(null);
+      };
+
+      //마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+      kakao.maps.event.addListener(marker[i], "click", function () {
+        // 오버레이 표시
+        overlay[i].setMap(createmap);
+        // 닫기버튼 오버레이 표시
+        overlay2[i].setMap(createmap);
+      });
+
+      // 마커가 지도 위에 표시되도록 설정합니다
+      marker[i].setMap(createmap);
+      markers.push(marker[i]);
+    }
+  };
 
   useEffect(() => {
     const script = document.createElement("script");
@@ -149,34 +233,12 @@ const MainMap = (props) => {
     script.onload = () => {
       kakao.maps.load(() => {
         /*
-         * 기존의 내 맛집 마커를 삭제
+         * 기존의 맛집 마커를 삭제
          */
-
         for (let i = 0; i < markers.length; i++) {
           markers[i].setMap(null);
         }
-        /*
-         * 기존의 친구 맛집 마커를 삭제
-         */
-        // for (let i = 0; i < friendMarkers.length; i++) {
-        //   friendMarkers[i].setMap(null);
-        // }
-
         setMap(createmap);
-        // // 나의 맛집 표시핀
-        // let imageSrc = myPinUrl[0], // 마커이미지의 주소입니다
-        //   // 마커이미지의 크기입니다
-        //   imageSize = new kakao.maps.Size(64, 69),
-        //   // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-        //   imageOption = { offset: new kakao.maps.Point(27, 69) };
-
-        // // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-        // let markerImage = new kakao.maps.MarkerImage(
-        //     imageSrc,
-        //     imageSize,
-        //     imageOption
-        //   ),
-        //   markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
 
         // 이미지 태그 제거
         let imgTag = /<IMG(.*?)>/gi;
@@ -304,11 +366,6 @@ const MainMap = (props) => {
           });
 
           setTimeout(() => {
-            // if (categoryFilter(myCategory[i])) {
-            //   marker[i].setMap(createmap);
-            //   markers.push(marker[i]);
-            // }
-
             if (
               userIdFilter(myUserId[i]) &&
               categoryFilter(myCategory[i]) &&
