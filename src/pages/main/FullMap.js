15d424a6897c4e6670f6aa6356838affb122509b
eatnows <gagyasindev@gmail.com>
2020-07-18 18:05:19 +0900

    채팅 내용 없을 시 전송 불가 ref #62

--- src/pages/main/FullMap.js
+++ src/pages/main/FullMap.js
@@ -431,7 +431,8 @@ class FullMap extends React.Component {
    * 메세지 전송
    */
   onClickMessageSend = (e) => {
-    // this.state.socket.send(this.state.sendMessage);
+    if (this.state.sendMessage === "") return;
+
     let socketMsg = `chat,${sessionStorage.getItem("userId")},${
       this.state.dmUserId
     },${this.state.sendMessage},${this.state.chatRoomId}`;
